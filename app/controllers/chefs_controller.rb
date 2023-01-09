13656e23c62258434bfc44c711b492a445989ad9
Patricia Severance <patriciakseverance@gmail.com>
2023-01-08 20:03:14 -0700

    Create ChefsController with update method, create view for chef show page with form for adding a dish, add route for chef show page and a patch request for chef update method, add tests

---
+++ app/controllers/chefs_controller.rb
@@ -0,0 +1,14 @@
+class ChefsController < ApplicationController
+    def show
+        @chef = Chef.find(params[:id])
+    end
+
+    def update
+        @chef = Chef.find(params[:id])
+        @dish = Dish.find(params[:add_dish])
+
+        @chef.dishes << @dish
+        
+        redirect_to "/chefs/#{@chef.id}"
+    end
+end
